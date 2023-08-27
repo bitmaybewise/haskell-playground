0d3c2f13c889d6b088e0cfbb76bb03c5cfc46423
Hercules Merscher <hlmerscher@gmail.com>
2023-08-27 19:48:41 +0200

    chapter 5: documenting modules

--- Chapter05/module-examples/src/HaskellBook/Examples/Introduction/CreatingModules.hs
+++ Chapter05/module-examples/src/HaskellBook/Examples/Introduction/CreatingModules.hs
@@ -1,3 +1,7 @@
+-- |
+-- This module serves as an example of how you can create a
+-- module. THis comment will be placed at the top of the generated
+-- documentation.
 module HaskellBook.Examples.Introduction.CreatingModules
   ( Name (..),
     Salutation (..),
@@ -12,12 +16,33 @@ data Name = Name {getName :: String}
 
 data Salutation = Salutation {getSalutation :: String}
 
+-- |
+-- A GreetingMessage contains all of the information needed to generate a
+-- greeting using 'formatMessage'. You can get a default greeting without
+-- attribution from 'defaultMessage'. This makes it convenient to use
+-- record update syntax to construct a new greeting:
+--
+-- >> formatMessage defaultMessage { greetingFrom = [ Name \"A Haskeller\" ] }
+-- \"Hello Friend, from A Haskeller\"
 data GreetingMessage = GreetingMessage
-  { greetingSalutation :: Salutation,
+  { -- | A 'Salutation', like \"Hello\"
+    greetingSalutation :: Salutation,
+    -- | 'Name' of the person that should be greeted
     greetingTo :: Name,
+    -- | 'Name's of the people who are sending the greeting
     greetingFrom :: [Name]
   }
 
+-- |
+-- A default greeting message that isn't attributed to anyone:
+--
+-- @
+-- `GreetingMessage`
+--   { `greetingSalutation` = Salutation \"Hello\"
+--   , `greetingTo` = Name \"Friend\"
+--   , `greetingFrom` = []
+--   }
+-- @
 defaultMessage :: GreetingMessage
 defaultMessage =
   GreetingMessage
@@ -57,6 +82,11 @@ formatMessage (GreetingMessage greetingSalutation greetingTo greetingFrom) =
         (friend : moreFriends) ->
           getName friend <> ", " <> formatFriendList moreFriends
 
+-- |
+-- A test message that you can use to see how messages are formatted.
+--
+-- >> testMessage
+-- "Hello Friend, from: test example"
 testMessage :: String
 testMessage =
   formatMessage $ defaultMessage {greetingFrom = [Name "test example"]}
