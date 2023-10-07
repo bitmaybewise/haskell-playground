e7d87d923c621e8b682974efabb4577e49d7433d
Hercules Merscher <hlmerscher@gmail.com>
2023-10-07 19:21:44 +0200

    chapter 13: basic state demo

---
+++ Chapter13/BasicStateDemo.hs
@@ -0,0 +1,22 @@
+module BasicStateDemo where
+
+import Control.Monad.State
+
+appendLineWithIndent :: String -> String -> State Int String
+appendLineWithIndent message previousMessage = do
+  indentLevel <- get
+  let nextIndentLevel = indentLevel + 2
+      indent = replicate nextIndentLevel ' '
+      output = previousMessage <> indent <> message <> "\n"
+  put nextIndentLevel
+  pure output
+
+appendLineDemo :: IO ()
+appendLineDemo =
+  putStrLn $ evalState message 0
+  where
+    message =
+      appendLineWithIndent "hello" ""
+        >>= appendLineWithIndent "world"
+        >>= appendLineWithIndent "love,"
+        >>= appendLineWithIndent "Fulano"
