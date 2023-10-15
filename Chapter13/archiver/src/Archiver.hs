e30db1d32ca8a0d3246562f1e244321b6d70b218
Hercules Merscher <hlmerscher@gmail.com>
2023-10-15 13:54:26 +0200

    chapter 13: building a file archiver

---
+++ Chapter13/archiver/src/Archiver.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Archiver where
+
+import Control.Applicative
+import Control.Monad.Except
+import Control.Monad.State
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import Data.Text qualified as Text
+
+data Archive = Archive
+  { archiveName :: Text,
+    archiveFiles :: [ArchivedFile]
+  }
+  deriving stock (Show)
+
+data ArchivedFile = ArchivedFile
+  { archivedFileName :: Text,
+    archivedFileContents :: ByteString
+  }
+  deriving stock (Show)
+
+newtype Archiver a = Archiver
+  {unArchiver :: StateT Text (ExceptT Text IO) a}
+  deriving newtype (Functor, Applicative, Monad, Alternative)
+
+runArchiver :: Text -> Archiver a -> IO (Either Text a)
+runArchiver inputText archiver =
+  runExceptT $ evalStateT (unArchiver archiver) inputText
+
+parseChar :: Archiver Char
+parseChar = do
+  parseText <- Archiver get
+  case Text.uncons parseText of
+    Nothing ->
+      Archiver . lift . throwError $ "end of input"
+    Just (c, rest) -> do
+      Archiver (put rest)
+      pure c
